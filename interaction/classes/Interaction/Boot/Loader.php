7acf159663ed78a8fda150e600b4e8cfda0becee
Alexandr Drobot <drobot.personal@gmail.com>
2024-07-30 23:17:37 +0300

    add php-pest

--- interaction/classes/Interaction/Boot/Loader.php
+++ interaction/classes/Interaction/Boot/Loader.php
@@ -37,8 +37,8 @@ class Loader
 
     public static function loadVendor(): void
     {
-        if (file_exists(__DIR__ . '/../../../../vendor/autoload.php')) {
-            require_once __DIR__ . '/../../../../vendor/autoload.php';
+        if (file_exists(__DIR__ . '/../../../vendor/autoload.php')) {
+            require_once __DIR__ . '/../../../vendor/autoload.php';
         }
     }
 
